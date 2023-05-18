1cae10c4917ac34ca265c335ef933cd57099c379
choewy <choewy32@gmail.com>
2023-05-19 01:26:02 +0900

    feature/bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,4 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Bootstrap } from './bootstrap';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+Bootstrap.run(AppModule);
